666a8ec81001b733f694a9f98d0a206b8e857a79
Anna Pestova <106414157+AnnaPestova1@users.noreply.github.com>
2024-03-10 19:32:08 -0400

    added theme provider

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,8 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
 import { ThemeProvider } from "@mui/material/styles";
-import { theme } from "@/app/theme";
+import { theme } from "@/styles/theme";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,7 +15,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ThemeProvider theme={theme}>{children}</ThemeProvider>
+        <AppRouterCacheProvider>
+          <ThemeProvider theme={theme}>{children}</ThemeProvider>
+        </AppRouterCacheProvider>
       </body>
     </html>
   );
