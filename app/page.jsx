eb8d0e3d67aa3c235976754cc5ce870584991181
Anna Pestova <106414157+AnnaPestova1@users.noreply.github.com>
2024-03-10 22:21:04 -0400

    fixed error

--- app/page.jsx
+++ app/page.jsx
@@ -5,11 +5,11 @@ export default async function Home() {
   await dbConnect();
   return (
     <Box p={5}>
-      <Typography color="primary" variant="h2">
-        Hello Next with{" "}
-        <Typography color="accent.main" variant="h1">
-          MongoDB
-        </Typography>
+      <Typography color="primary" variant="h1">
+        Hello Next with
+      </Typography>
+      <Typography color="accent.main" variant="h2">
+        MongoDB
       </Typography>
     </Box>
   );
