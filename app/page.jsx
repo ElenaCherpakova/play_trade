22eda66321dd1a7d62db2f98e2f30ce2a19ce863
Liubov Rodin <lyubov.lysenko@gmail.com>
2024-03-11 11:29:06 -0400

    Created reusable CardComponent

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,17 @@
 import dbConnect from "@/lib/mongo/dbConnect";
 import CardComponent from "../components/CardComponent";
 
+const card = {
+  name: "Pikachu V - SWSH061",
+  price: "$ 0.42",
+  imageURL: "https://m.media-amazon.com/images/I/51skd-tjunL._AC_.jpg"
+};
 export default async function Home() {
   await dbConnect();
   return (
     <div>
       <h1>Hello Next with MongoDB</h1>
-      <CardComponent />
+      <CardComponent card={card} />
     </div>
   );
 }
--- components/CardComponent.jsx
+++ components/CardComponent.jsx
@@ -6,29 +6,35 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
-export default function CardComponent() {
+import Link from "@mui/material/Link";
+
+export default function CardComponent({ card }) {
   return (
     <Card variant="outlined" style={{ border: "none", maxWidth: 220 }}>
       <CardActionArea>
-        <CardMedia
-          // style={{ objectFit: "contain" }}
-          style={{ objectFit: "cover", padding: 10 }}
-          component="img"
-          image="https://m.media-amazon.com/images/I/51skd-tjunL._AC_.jpg"
-          alt="Pikachu V - SWSH061"
-        />
-        <CardContent sx={{ p: 1 }}>
+        <Link href={`/market/item/${card._id}`}>
+          {/* <Link href={`/card/${card._id}`}> */}
+          <CardMedia
+            // style={{ objectFit: "contain" }}
+            style={{ objectFit: "cover", padding: 8 }}
+            component="img"
+            image={card.imageURL}
+            alt={card.name}
+          />
+        </Link>
+        <CardContent sx={{ p: 0.5 }}>
           <div style={{ display: "flex", justifyContent: "space-between" }}>
-            <Typography gutterBottom variant="h8" component="div">
-              Pikachu V - SWSH061
+            <Typography gutterBottom variant="caption" component="div">
+              {card.name}
             </Typography>
-            <Typography gutterBottom variant="h7" component="div">
-              $ 0.42
+            <Typography gutterBottom variant="caption" component="div">
+              {card.price}
             </Typography>
           </div>
         </CardContent>
       </CardActionArea>
-      <CardActions>
+
+      <CardActions sx={{ p: 0.5 }}>
         <Button size="small" variant="contained" color="secondary">
           Buy Now
         </Button>
