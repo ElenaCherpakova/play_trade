cadd9d3585398b80d0014d592193583e60e63682
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2024-03-10 18:11:55 -0700

    add home title

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,10 @@
-import dbConnect from '@/lib/mongo/dbConnect';
+import dbConnect from "@/lib/mongo/dbConnect";
 
 export default async function Home() {
   await dbConnect();
   return (
     <div>
-      <h1>Hello Next with MongoDB</h1>
+      <h1>HOME - Hello Next with MongoDB</h1>
     </div>
   );
 }
