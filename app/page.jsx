efbdbee4c058799a53d6683c7341f2338ee16f9e
Victoria240 <taiwovictoria024@gmail.com>
2024-03-13 00:12:19 +0000

    navbar component
    Unstyled navbar component

--- app/page.jsx
+++ app/page.jsx
@@ -1,17 +1,20 @@
 import dbConnect from "@/lib/mongo/dbConnect";
 import { Typography, Button, TextField, Box } from "@mui/material";
-
+import Navbar from '../components/Navbar';
 
 export default async function Home() {
   await dbConnect();
   return (
     <Box p={5}>
+      <Navbar />
       <Typography color="primary" variant="h1">
         Hello Next with
       </Typography>
       <Typography color="accent.main" variant="h2">
         MongoDB
       </Typography>
+      
     </Box>
+    
   );
 }
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,7 +1,57 @@
-export default function Navbar() {
+// components/Navbar.js
+
+'use client';
+import Link from 'next/link';
+
+const Navbar = () => {
+  const handleSearch = (event) => {
+    // Implement search functionality here
+    console.log('Searching for:', event.target.value);
+  };
   return (
-    <nav>
-      <h2>Navbar</h2>
-    </nav>
+    <header className="header">
+      <div className="logo">
+        <Link href="/">
+          Play Trade
+        </Link>
+      </div>
+
+      <div className="search">
+        <input
+          type="text"
+          placeholder="Search cards..."
+          onChange={handleSearch}
+        />
+      </div>
+
+      <nav className="nav">
+        <ul>
+          <li>
+            <Link href="/cards">
+              Cards
+            </Link>
+          </li>
+          <li>
+            <Link href="/about">
+              About
+            </Link>
+          </li>
+          <li>
+            <Link href="/signup">
+              Signup
+            </Link>
+          </li>
+          <li>
+            <Link href="/login">
+              Login
+            </Link>
+          </li>
+          {/* Add more navigation links as needed */}
+        </ul>
+      </nav>
+
+    </header>
   );
-}
+};
+
+export default Navbar;
