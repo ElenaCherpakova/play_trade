d840a0245d9fd9971a865cb8198236dca032b701
ElenaCherpakova <elena.cherpakova@gmail.com>
2024-03-13 00:55:58 -0400

    fix minor issues

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -8,10 +8,7 @@ import dbConnect from "@/lib/mongo/dbConnect";
 export const authOptions = {
   providers: [
     CredentialsProvider({
-      credentials: {
-        email: { label: "email", type: "text" },
-        password: { label: "password", type: "password" }
-      },
+      credentials: {},
       async authorize(credentials) {
         const { email, password } = credentials;
         try {
@@ -19,25 +16,18 @@ export const authOptions = {
 
           // Check if the user already exists in the database
           let user = await User.findOne({ email });
-          console.log(user);
           if (!user) {
-            // If the user does not exist, create a new user
-            user = new User({
-              email: email,
-              password: password // You may want to hash the password here before saving it
-            });
-            await user.save();
-          } else {
-            // If the user exists, validate the password
-            const passwordMatch = await user.comparePassword(password);
-            if (!passwordMatch) {
-              return null; // Return null if the password does not match
-            }
+            throw new Error("User with this email doesn't exist");
+          }
+          // If the user exists, validate the password
+          const isPasswordCorrect = await user.comparePassword(password);
+          if (!isPasswordCorrect) {
+            throw new Error("Invalid email or password");
           }
 
-          return user; // Return the user if authentication is successful
+          return user;
         } catch (error) {
-          throw new Error(error);
+          throw new Error("Authentication failed");
         }
       }
     }),
@@ -48,26 +38,26 @@ export const authOptions = {
   ],
 
   callbacks: {
-    async signIn({ user, account }) {
-      console.log(user, account);
+    async signIn({ profile: { sub }, user, account }) {
       if (account.provider === "google") {
         try {
-          const { name, email, sub } = user;
+          const { name, email } = user;
 
           await dbConnect();
           const userExist = await User.findOne({ email });
           const hashPassword = await bcrypt.hashSync(sub, 10);
-          if (!userExist) {
-            const newUser = new User({
-              name: name,
-              email: email,
-              password: hashPassword,
-              authProvider: true // Setting authProvider to true specifically for Google registration
-            });
-            const res = await newUser.save();
-            if (res.status === 200 || res.status === 201) {
-              return user;
-            }
+          if (userExist) {
+            return user;
+          }
+          const newUser = new User({
+            name: name,
+            email: email,
+            password: hashPassword,
+            authProvider: true // Setting authProvider to true specifically for Google registration
+          });
+          const res = await newUser.save();
+          if (res.status === 200 || res.status === 201) {
+            return user;
           }
         } catch (error) {
           console.log(error);
@@ -79,16 +69,16 @@ export const authOptions = {
       if (user) {
         token.email = user.email;
         token.name = user.name;
-        token.sub = user.sub;
-        token.authProvider = user.authProvider; // Adding authProvider to the token
+        token.sub;
       }
       return token;
     },
     async session({ session, token }) {
-      session.user.email = token.email;
-      session.user.name = token.name;
-      session.user.sub = token.sub;
-      session.user.authProvider = token.authProvider; // Adding authProvider to the session
+      if (session.user) {
+        session.user.email = token.email;
+        session.user.name = token.name;
+        session.user.sub = token.sub;
+      }
       return session;
     }
   },
