bcfa0741c896497029945edbf9a412e9d0021b79
AnnaSolovykh <ms.teachy.solo@gmail.com>
2024-03-15 15:45:52 -0400

    add sign up page

--- app/signup/page.jsx
+++ app/signup/page.jsx
@@ -1,7 +1,143 @@
-export default function Signup() {
+'use client'
+
+import { useRouter } from "next/navigation";
+import { useState } from 'react';
+import useAuthUser from '../../store/useAuthUser';
+import { Container, Box, TextField, Button, Typography, CircularProgress, Link, InputAdornment, IconButton, Divider } from '@mui/material';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+
+const SignUpPage = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+  });
+  const [showPassword, setShowPassword] = useState(false);
+
+  const { register, isLoading, error } = useAuthUser();
+  const router = useRouter();
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleClickShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    register(formData);
+  };
+
+  const handleLoginRedirect = () => {
+    router.push('/signin'); 
+  };
+
   return (
-    <div>
-      <h2>Signup</h2>
-    </div>
+    <Container component="main" maxWidth="xs">
+      <Box
+        sx={{
+          marginTop: 8,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <Typography component="h1" variant="h5">
+          Sign up to PlayTrade
+        </Typography>
+        {error && <Typography color="error">Error: {error}</Typography>}
+        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="name"
+            label="Username"
+            name="name"
+            autoComplete="name"
+            autoFocus
+            value={formData.name}
+            onChange={handleChange}
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            value={formData.email}
+            onChange={handleChange}
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type={showPassword ? 'text' : 'password'}
+            id="password"
+            autoComplete="current-password"
+            value={formData.password}
+            onChange={handleChange}
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={handleClickShowPassword}
+                    onMouseDown={handleMouseDownPassword}
+                    edge="end"
+                  >
+                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                  </IconButton>
+                </InputAdornment>
+              ),
+            }}
+          />
+          <Typography variant="caption" display="block" gutterBottom>
+            <Link href="#" disabled variant="caption" sx={{ float: 'right' }} underline="none">
+              Forgot password?
+            </Link>
+          </Typography>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+            disabled={isLoading}
+          >
+            {isLoading ? <CircularProgress size={24} /> : 'Sign Up'}
+          </Button>
+          <Box sx={{ display: 'flex', alignItems: 'center', my: 2 }}>
+            <Divider sx={{ flexGrow: 1 }} />
+            <Typography sx={{ mx: 2, color: 'text.secondary' }}>or</Typography>
+            <Divider sx={{ flexGrow: 1 }} />
+          </Box> 
+          <Button
+            onClick={handleLoginRedirect}
+            fullWidth
+            variant="contained"
+            sx={{ mt: 2, mb: 2 }}
+          >
+            Login with an existing aacount
+          </Button> 
+        </Box>
+      </Box>
+    </Container>
   );
-}
+};
+
+export default SignUpPage;
