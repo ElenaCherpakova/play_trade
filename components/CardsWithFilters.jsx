7a4af23692f77651a8594ed851cea3125e3eacfa
Liubov Rodin <lyubov.lysenko@gmail.com>
2024-03-18 01:31:18 -0400

    added two select components

--- components/CardsWithFilters.jsx
+++ components/CardsWithFilters.jsx
@@ -1,7 +1,7 @@
 "use client";
-import { useState } from "react";
-import { Grid, Paper, Typography, FormControl, InputLabel, Select, MenuItem } from "@mui/material";
-import { SelectChangeEvent } from "@mui/material";
+import { useEffect, useState } from "react";
+import { Grid, Paper, Typography, FormControl, InputLabel, Select, MenuItem, Box } from "@mui/material";
+// import { SelectChangeEvent } from "@mui/material";
 import CardComponent from "./CardComponent";
 
 // const currentCards = cards.slice(indexOfFirstItem, indexOfLastItem);
@@ -11,35 +11,94 @@ const card = {
   imageURL: "https://m.media-amazon.com/images/I/51skd-tjunL._AC_.jpg"
 };
 
-const [category, setCategory] = useState("");
-
 // const handleChange = (event: SelectChangeEvent) => {
 //   const newValue = event.target.value; // Assuming value is a string
 //   setCategory(newValue);
 // };
 const CardsWithFilters = ({ cards }) => {
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const [selectedCategory, setSelectedCategory] = useState("");
+  const [selectedConditions, setSelectedConditions] = useState("");
+
+  const handleSelectCategoryChange = event => {
+    const selectedCardCategory = event.target.value;
+    setSelectedCategory(selectedCardCategory);
+  };
+  const handleSelectConditionsChange = event => {
+    const selectedCardConditions = event.target.value;
+    setSelectedConditions(selectedCardConditions);
+  };
+  // useEffect for Conditions
+
+  useEffect(() => {
+    const fetchConditions = async () => {
+      if (selectedCategory === "Sport Card") {
+        try {
+          await getSportCardConditions(dispatchSportCardConditions);
+        } catch (error) {
+          setErrorMessage("");
+          setErrorMessage("Error loading sport card conditions");
+        }
+      } else {
+        try {
+          await getGameCardConditions(dispatchGameCardConditions);
+        } catch (error) {
+          setErrorMessage("");
+          setErrorMessage("Error loading game card conditions");
+        }
+      }
+    };
+    fetchConditions();
+  }, [selectedCategory]);
+
   return (
     <>
       <Grid container spacing={2}>
         {/* Filters Section*/}
+
         <Grid item xs={6} md={3}>
           <Paper>
             <Typography variant="h4" align="center">
               Filters
             </Typography>
-            <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
-              <InputLabel id="label">Category</InputLabel>
-              <Select labelId="label" id="select" value={category} onChange={handleChange} label="Category">
-                <MenuItem value="">
-                  <em>None</em>
-                </MenuItem>
-                <MenuItem value={10}>Magic</MenuItem>
-                <MenuItem value={20}>Pokemon</MenuItem>
-                <MenuItem value={30}>Digimon</MenuItem>
-                <MenuItem value={30}>Yu-Gi-Oh!</MenuItem>
-                <MenuItem value={30}>Sport Card</MenuItem>
-              </Select>
-            </FormControl>
+            <Box display="flex" flexDirection="column" padding="1rem">
+              <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
+                <InputLabel id="label">Category</InputLabel>
+                <Select
+                  labelId="label"
+                  id="select"
+                  value={selectedCategory}
+                  onChange={handleSelectCategoryChange}
+                  label="Category">
+                  <MenuItem value="">
+                    <em>None</em>
+                  </MenuItem>
+                  <MenuItem value={10}>Magic</MenuItem>
+                  <MenuItem value={20}>Pokemon</MenuItem>
+                  <MenuItem value={30}>Digimon</MenuItem>
+                  <MenuItem value={30}>Yu-Gi-Oh!</MenuItem>
+                  <MenuItem value={30}>Sport Card</MenuItem>
+                </Select>
+              </FormControl>
+              <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
+                <InputLabel id="label">Conditions</InputLabel>
+                <Select
+                  labelId="label"
+                  id="select"
+                  value={selectedConditions}
+                  onChange={handleSelectConditionsChange}
+                  label="Conditions">
+                  <MenuItem value="">
+                    <em>None</em>
+                  </MenuItem>
+                  <MenuItem value={10}>near mint</MenuItem>
+                  <MenuItem value={20}>excellent</MenuItem>
+                  <MenuItem value={30}>very good</MenuItem>
+                  <MenuItem value={30}>poor</MenuItem>
+                </Select>
+              </FormControl>
+            </Box>
           </Paper>
         </Grid>
         {/* Cards Section*/}
