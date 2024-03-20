083599b3c5bef6a39aa27f936e90f60da884a37e
AnnaSolovykh <ms.teachy.solo@gmail.com>
2024-03-20 17:58:06 -0400

    add mailer config

---
+++ utils/mailerConfig.js
@@ -0,0 +1,37 @@
+const { google } = require("googleapis");
+const nodemailer = require("nodemailer");
+
+const EMAIL = process.env.MAIL_USERNAME;
+const CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+const CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
+const REDIRECT_URI = process.env.REDIRECT_URI;
+const REFRESH_TOKEN = process.env.GOOGLE_CLIENT_REFRESH_TOKEN;
+
+const oAuth2Client = new google.auth.OAuth2(
+    CLIENT_ID,
+    CLIENT_SECRET,
+    REDIRECT_URI
+);
+oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
+
+const createTransporter = async () => {
+    try { 
+        const { token: accessToken } = await oAuth2Client.getAccessToken();
+        const transporter = nodemailer.createTransport({
+            service: "gmail",
+            auth: {
+                type: "OAuth2",
+                user: EMAIL,
+                clientId: CLIENT_ID,
+                clientSecret: CLIENT_SECRET,
+                refreshToken: REFRESH_TOKEN,
+                accessToken,
+            },
+        });
+        return transporter;
+    } catch (error){
+        throw error; 
+    }
+};
+
+module.exports = createTransporter;
