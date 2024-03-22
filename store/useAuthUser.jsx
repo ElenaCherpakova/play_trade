c3c8121ab3b8f719cb7c23d13d2c7d7ec1a733a3
ElenaCherpakova <elena.cherpakova@gmail.com>
2024-03-22 17:56:51 -0400

    add callback url once user login using google

--- store/useAuthUser.jsx
+++ store/useAuthUser.jsx
@@ -1,19 +1,19 @@
-import  { create } from 'zustand';
-import { signOut, signIn } from "next-auth/react"
+import { create } from "zustand";
+import { signOut, signIn } from "next-auth/react";
 
-const useAuthUser = create((set) => ({
-    email: '',
-    verifyError: '',
+const useAuthUser = create(set => ({
+  email: "",
+  verifyError: "",
   isLoading: false,
   error: null,
-  passwordResetSuccess: false, 
+  passwordResetSuccess: false,
 
-  login: async (userData) => {
-    const { email, password } = userData
+  login: async userData => {
+    const { email, password } = userData;
     set({ isLoading: true, error: null });
     try {
-      const response = await signIn('credentials', { 
-        email, 
+      const response = await signIn("credentials", {
+        email,
         password,
         redirect: false,
         callbackUrl: `${window.location.origin}/market`
@@ -22,9 +22,9 @@ const useAuthUser = create((set) => ({
         let errorMessage = "";
         if (response.error === "CredentialsSignin") {
           errorMessage = "please check your login details and try again.";
-        } 
+        }
         set({ error: errorMessage, isLoading: false });
-      } 
+      }
     } catch (error) {
       set({ error: error.message, isLoading: false });
     }
@@ -32,7 +32,7 @@ const useAuthUser = create((set) => ({
 
   logout: async () => {
     try {
-      await signOut({ 
+      await signOut({
         callbackUrl: `${window.location.origin}/signin`
       });
     } catch (error) {
@@ -40,51 +40,53 @@ const useAuthUser = create((set) => ({
     }
   },
 
-  register: async (newUserData) => {
+  register: async newUserData => {
     set({ isLoading: true, error: null });
     try {
-      const response = await fetch('/api/register', {
-            method: 'POST',
-            headers: {
-            'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(newUserData),
-        });
-        const data = await response.json();
-        if (response.ok) {
-            set({ isLoading: false, error: null });
-            window.location.href = '/signin';
-        } else {
-            throw new Error(data.error || 'Registration failed');
-        }
+      const response = await fetch("/api/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(newUserData)
+      });
+      const data = await response.json();
+      if (response.ok) {
+        set({ isLoading: false, error: null });
+        window.location.href = "/signin";
+      } else {
+        throw new Error(data.error || "Registration failed");
+      }
     } catch (error) {
       set({ error: error.message, isLoading: false });
     }
   },
   googleLogin: async () => {
     try {
-      await signIn("google");
+      await signIn("google", {
+        callbackUrl: `${window.location.origin}/market`
+      });
     } catch (error) {
       set({ error: error.message, isLoading: false });
     }
   },
-  forgetPassword: async (userData) => {
+  forgetPassword: async userData => {
     set({ isLoading: true, error: null });
     try {
-      const response = await fetch('/api/forget-password', {
-            method: 'POST',
-            headers: {
-            'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(userData),
-        });
-        const data = await response.json();
-        if (response.ok) {
-            set({ isLoading: false, error: null });
-            window.location.href = '/signin';
-        } else {
-            throw new Error(data.error || 'Resetting the password failed');
-        }
+      const response = await fetch("/api/forget-password", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(userData)
+      });
+      const data = await response.json();
+      if (response.ok) {
+        set({ isLoading: false, error: null });
+        window.location.href = "/signin";
+      } else {
+        throw new Error(data.error || "Resetting the password failed");
+      }
     } catch (error) {
       set({ error: error.message, isLoading: false });
     }
@@ -92,30 +94,30 @@ const useAuthUser = create((set) => ({
   resetPassword: async (password, email) => {
     set({ isLoading: true, error: null, passwordResetSuccess: false });
     try {
-      const response = await fetch('/api/reset-password', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ password, email }),
+      const response = await fetch("/api/reset-password", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ password, email })
       });
 
       if (response.ok) {
         set({ isLoading: false, passwordResetSuccess: true });
       } else {
         const errorData = await response.json();
-        console.log(errorData)
-        set({ isLoading: false, error: errorData.error || 'Resetting the password failed' });
+        console.log(errorData);
+        set({ isLoading: false, error: errorData.error || "Resetting the password failed" });
       }
     } catch (error) {
-        set({ isLoading: false, error: error.message, passwordResetSuccess: false });
+      set({ isLoading: false, error: error.message, passwordResetSuccess: false });
     }
   },
-  verifyToken: async (token) => {
+  verifyToken: async token => {
     set({ isLoading: true, error: null });
     try {
-      const response = await fetch('/api/verify-token', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ token }),
+      const response = await fetch("/api/verify-token", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ token })
       });
 
       if (response.ok) {
@@ -128,6 +130,6 @@ const useAuthUser = create((set) => ({
     } catch (error) {
       set({ isLoading: false, verifyError: error.message });
     }
-  },
+  }
 }));
 export default useAuthUser;
