ea92b8e7fde3a78ef0bbd8d43801f40fb618f1e4
Anna Pestova <106414157+AnnaPestova1@users.noreply.github.com>
2024-03-24 11:44:53 -0400

    add route to checkout

--- app/cart/page.jsx
+++ app/cart/page.jsx
@@ -1,7 +1,13 @@
+"use client";
+import { Button } from "@mui/material";
+import { useRouter } from "next/navigation";
 export default function Cart() {
+  const router = useRouter();
   return (
     <div>
-      <h2>Cart</h2>
+      <Button variant="contained" color="primary" onClick={() => router.push("/cart/checkout")}>
+        Checkout
+      </Button>
     </div>
   );
 }
