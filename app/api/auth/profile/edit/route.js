fac5465e2a448c31e51673bbb83effbcaadc310c
ElenaCherpakova <elena.cherpakova@gmail.com>
2024-03-23 20:07:12 -0400

    remove consoles

--- app/api/auth/profile/edit/route.js
+++ app/api/auth/profile/edit/route.js
@@ -28,10 +28,6 @@ export const PATCH = async (req, res) => {
     if (!name || !email || !location || !trigger) {
       return NextResponse.json({ success: false, message: "Name and Email are required" }, { status: 400 });
     }
-    // console.log("NAME", name);
-    // console.log("Email", email);
-    // console.log("location", location);
-    // console.log("userId", userId);
     const updateUser = await User.findByIdAndUpdate(userId, body, {
       new: true,
       runValidators: true
@@ -40,7 +36,7 @@ export const PATCH = async (req, res) => {
       return NextResponse.json({ success: false, message: "Profile not found" }, { status: 400 });
     }
 
-    if (trigger === "update" && session.user._id) {
+    if (trigger === "update" && session?.user?._id) {
       session.user = updateUser;
     }
     return NextResponse.json({ success: true, message: "Your profile is updated", data: updateUser }, { status: 200 });
