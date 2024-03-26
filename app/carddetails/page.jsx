4c45b60cab980ebb043d89bd094fb71f11d97584
Victoria240 <taiwovictoria024@gmail.com>
2024-03-26 19:41:21 +0000

    Added more details and styling

--- app/carddetails/page.jsx
+++ app/carddetails/page.jsx
@@ -1,77 +1,101 @@
 'use client'
 import * as React from "react";
-import { useRouter } from "next/navigation";
-import { Typography, Button, Grid, Card, CardContent, CardMedia } from "@mui/material";
+import { Typography, Button, Card, CardMedia, ThemeProvider, Breadcrumbs, Link } from "@mui/material";
+import { useTheme } from "@mui/material/styles";
+import { useRouter } from 'next/navigation';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import FavoriteIcon from '@mui/icons-material/Favorite';
 
 const CardDetailsPage = () => {
+    const router = useRouter();
+    const theme = useTheme();
+
     // Hard-coded card details
-    const carddetails = {
+    const cardDetails = {
         name: "Pokemon Card",
         imageURL: "/images/pokemon.jpg",
-        description: "This is a rare and powerful Pokemon card.",
+        description: "This is a rare and powerful Pokemon card. It's a must-have for any serious collector.",
         price: "$10",
-    };
-
-    const router = useRouter();
-
-    // Function to handle the back button
-    const handleBack = () => {
-        router.back(); // Navigate back to the previous page
+        rarity: "Rare",
+        category: "Pokemon",
+        condition: "Mint",
+        seller: "John Doe",
+        location: "New York, USA",
     };
 
     return (
-        <Grid container spacing={2}>
-            <Grid item xs={12}>
-                <Button variant="contained" color="primary" onClick={handleBack}>
-                    Back to Listings
-                </Button>
-                {/* You can add more action buttons here, like "Add to Cart" or "Buy Now" */}
-            </Grid>
-            {/* Item Image */}
-            <Grid item xs={12} sm={6} md={4} lg={3}>
-                <Card>
-                    <CardMedia
-                        component="img"
-                        image="/images/pokemon.jpg"
-                        alt={carddetails.name}
-                    />
-                </Card>
-            </Grid>
+        <ThemeProvider theme={theme}>
+            <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'left' }}>
+                {/* Breadcrumbs */}
+                <Breadcrumbs aria-label="breadcrumb" style={{ marginTop: theme.spacing(2) }}>
+                    <Link color="inherit" href="/" onClick={() => router.push('/')}>
+                        Home
+                    </Link>
+                    <Typography color="text.primary">Card Details</Typography>
+                </Breadcrumbs>
 
-            {/* Item Details */}
-            <Grid item xs={12} sm={6} md={8} lg={9}>
-                <Typography variant="h4" gutterBottom>
-                    {carddetails.name}
-                </Typography>
-                <Typography variant="subtitle1" gutterBottom>
-                    Price: {carddetails.price}
-                </Typography>
-                <Typography variant="body1" gutterBottom>
-                    Description: {carddetails.description}
-                </Typography>
-                {/* Additional details can be added here */}
-                <Grid item xs={12}>
-                    <Button variant="contained" color="primary" onClick={handleBack}>
-                        Add to cart
-                    </Button>
-                    {/* You can add more action buttons here, like "Add to Cart" or "Buy Now" */}
-                </Grid>
-                <Grid item xs={12}>
-                    <Button variant="contained" color="primary" onClick={handleBack}>
-                        Add to favourites
-                    </Button>
-                    {/* You can add more action buttons here, like "Add to Cart" or "Buy Now" */}
-                </Grid>
-            </Grid>
+                {/* Image and Details Section */}
+                <div style={{ display: 'flex', marginTop: theme.spacing(2) }}>
+                    {/* Image Section */}
+                    <Card style={{ boxShadow: 'none', marginRight: theme.spacing(2) }}>
+                        <CardMedia
+                            component="img"
+                            image={cardDetails.imageURL}
+                            alt={cardDetails.name}
+                            style={{ width: 300, height: "auto" }}
+                        />
+                    </Card>
 
-            {/* Action Buttons */}
-            <Grid item xs={12}>
-                <Button variant="contained" color="primary" onClick={handleBack}>
-                    Add to cart
-                </Button>
-                {/* You can add more action buttons here, like "Add to Cart" or "Buy Now" */}
-            </Grid>
-        </Grid>
+                    {/* Details Section */}
+                    <div style={{ maxWidth: 600, paddingLeft: theme.spacing(2), borderRadius: theme.shape.borderRadius }}>
+                        <Typography variant="h4" gutterBottom>
+                            {cardDetails.name}
+                        </Typography>
+                        <Typography variant="subtitle1" gutterBottom>
+                            Price: {cardDetails.price}
+                        </Typography>
+                        <Typography variant="body1" gutterBottom>
+                            Description: {cardDetails.description}
+                        </Typography>
+                        <Typography variant="body1" gutterBottom>
+                            Rarity: {cardDetails.rarity}
+                        </Typography>
+                        <Typography variant="body1" gutterBottom>
+                            Category: {cardDetails.category}
+                        </Typography>
+                        <Typography variant="body1" gutterBottom>
+                            Condition: {cardDetails.condition}
+                        </Typography>
+                        <Typography variant="body1" gutterBottom>
+                            Seller: {cardDetails.seller}
+                        </Typography>
+                        <Typography variant="body1" gutterBottom>
+                            Location: {cardDetails.location}
+                        </Typography>
+
+                        {/* Action Buttons */}
+                        <div style={{ display: 'flex', gap: theme.spacing(1), marginTop: theme.spacing(2) }}>
+                            <Button
+                                variant="contained"
+                                color="accent"
+                                style={{ color: theme.palette.background.paper }}
+                                startIcon={<ShoppingCartIcon />}
+                            >
+                                Add to Cart
+                            </Button>
+                            <Button
+                                variant="contained"
+                                color="secondary"
+
+                                startIcon={<FavoriteIcon />}
+                            >
+                                Add to Favorites
+                            </Button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </ThemeProvider>
     );
 };
 
