24d6a4dabb05e79ffe5ccc262dc1cb4f68326451
Oksana Fetter <fetter.oksana@gmail.com>
2024-03-28 22:30:36 -0400

    fix typos and remove use effect

--- components/CardsHome.jsx
+++ components/CardsHome.jsx
@@ -1,38 +1,23 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Link from "next/link";
 import { Grid, Card, CardContent, CardMedia, Typography } from "@mui/material";
 
 export default function CardsHome() {
-  const [cards, setCards] = useState([]);
-
-  useEffect(() => {
-    fetch("/api/cards") // Fetch data from the API
-      .then(response => response.json())
-      .then(data => {
-        // Check if data is an array
-        if (Array.isArray(data)) {
-          setCards(data);
-        } else {
-          // If data is not an array, transform it into an array
-          setCards([data]);
-        }
-      });
-  }, []);
   return (
     <Grid
       container
       spacing={2}
       justifyContent="center"
       sx={{ p: { xs: 2, sm: 0 }, flexDirection: { xs: "column", sm: "row" } }}>
-      {["Magic", "Pokemon", "Digimon", "Yu-Gi-Oh", "Sport Cards"].map((cardType, index) => (
+      {["Magic", "Pokemon", "Digimon", "YuGiOh", "Sport Card"].map((cardType, index) => (
         <Grid item xs={12} sm={6} md={4} lg={2} key={index} sx={{ height: "100%" }}>
           <Link href={`/market?type=${encodeURIComponent(cardType)}`} style={{ textDecoration: "none"}}>
             <Card sx={{ display: "flex", flexDirection: "column", height: "200px", width: "60%" }}>
               <CardMedia
                 component="img"
                 sx={{ height: "80%", objectFit: "cover" }}
-                image={`/cards/${cardType === "Sport Cards" ? "sport" : cardType.toLowerCase()}.png`}
+                image={`/cards/${cardType === "Sport Card" ? "sport" : cardType.toLowerCase().replace(/\s+/g, '')}.png`}
                 alt={cardType}
               />
               <CardContent>
