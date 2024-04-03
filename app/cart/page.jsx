4c78e755c04f2813297121abc417ecc6abab8a38
Oksana Fetter <fetter.oksana@gmail.com>
2024-04-03 02:05:41 -0400

    Refactor AddToCartButton and Cart page components

--- app/cart/page.jsx
+++ app/cart/page.jsx
@@ -1,27 +1,17 @@
 "use client";
-import React, { createContext, useState } from "react";
+import React from "react";
 import { Button } from "@mui/material";
 import { useRouter } from "next/navigation";
 import ShoppingCart from "@/components/ShoppingCart";
-//import { createContext } from "react";
-
-export const CartContext = createContext();
 
 export default function Cart() {
   const router = useRouter();
-  const [cartItems, setCartItems] = useState([]);
-
-  const addToCart = item => {
-    setCartItems(prevItems => [...prevItems, item]);
-  };
 
   return (
-    <CartContext.Provider value={{ cartItems, addToCart }}> 
-      <ShoppingCart>
-        <Button variant="contained" color="accent" onClick={() => router.push("/cart/checkout")} fullWidth>
-          Checkout
-        </Button>
-      </ShoppingCart>
-    </CartContext.Provider>
+    <ShoppingCart>
+      <Button variant="contained" color="accent" onClick={() => router.push("/cart/checkout")} fullWidth>
+        Checkout
+      </Button>
+    </ShoppingCart>
   );
 }
--- components/AddToCartButton.jsx
+++ components/AddToCartButton.jsx
@@ -1,16 +1,14 @@
-import React, { useContext } from "react";
+import React from "react";
 import Button from "@mui/material/Button";
 import { useRouter } from "next/navigation";
-import { CartContext } from "/app/cart/page";
 
 const AddToCartButton = ({ card }) => {
-  const { addToCart } = useContext(CartContext);//consuming the addToCart function from the CartContext
   const router = useRouter();
 
   const handleAddToCart = () => {
     addToCart(card);
     console.log("Product added to cart");
-    router.push('/cart'); // Redirect to the shopping cart
+    router.push(`/cart?id=${card.id}`); // Redirect to the shopping cart
   };
 
   return (
