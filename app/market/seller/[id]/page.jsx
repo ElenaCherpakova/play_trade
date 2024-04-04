bb133036dc58b237aef2f6b9cd8c74ded57df45e
Anna Pestova <106414157+AnnaPestova1@users.noreply.github.com>
2024-04-04 18:56:18 -0400

    fixed layout of seller page and theme

--- app/market/seller/[id]/page.jsx
+++ app/market/seller/[id]/page.jsx
@@ -1,13 +1,10 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { useTheme } from "@mui/material";
-import { useRouter } from "next/navigation";
 import { useSession } from "next-auth/react";
 import { fetchSellerCards } from "@/utils/fetchData";
 import CardComponent from "@/components/CardComponent";
 import { Alert, Avatar, Box, Container, Grid, Paper, Snackbar, Tab, Tabs, Typography } from "@mui/material";
-import { set } from "mongoose";
 
 const user = {
   name: "Our new seller",
@@ -16,9 +13,8 @@ const user = {
   isSeller: true
 };
 const seller = {
-  // userId: "123456",
   rating: 4.5,
-  feedback: 100,
+  feedback: ["Great seller", "Fast shipping", "Good communication", "Highly recommended"],
   numberOfSales: 2,
   location: 100
 };
@@ -27,15 +23,10 @@ export default function Seller({ params }) {
   const [errorMessage, setErrorMessage] = useState("");
   const [activeTab, setActiveTab] = useState("cards"); //for tabs
   const [cards, setCards] = useState([]); //for cards
-  const router = useRouter();
-  const theme = useTheme();
   const { data: session, status } = useSession();
   const showButtons = false;
   //for now fot testing, later it will be a seller id
   const userId = session?.user?._id;
-  console.log("session", session);
-  console.log("status", status);
-  console.log("userId", userId);
 
   useEffect(() => {
     if (userId) {
@@ -69,7 +60,6 @@ export default function Seller({ params }) {
             display: "flex",
             flexDirection: "column",
             gap: 1,
-            // justifyContent: { xs: "center", sm: "flex-start" },
             backgroundColor: "background.default",
             justifyContent: "center",
             alignItems: { xs: "center", sm: "flex-start" },
@@ -85,27 +75,18 @@ export default function Seller({ params }) {
               justifyContent: "center",
               alignItems: "center"
             }}>
-            <Avatar alt="seller image" src={session?.user?.avatarImgURL} sx={{ width: 100, height: 100 }} />
-            <Typography variant="h2">{user.name}</Typography>
-          </Box>
-          <Box
-            sx={{
-              display: "flex",
-              flexDirection: "row",
-              gap: 5,
-              width: "100%"
-              // justifyContent: "center",
-              // alignItems: "center"
-            }}>
-            <Grid container spacing={2}>
-              <Grid item xs></Grid>
-              <Grid item sm={1}>
+            <Box>
+              <Avatar alt="seller image" src={session?.user?.avatarImgURL} sx={{ width: 100, height: 100 }} />
+            </Box>
+            <Box gap={2} display="flex" flexDirection="column">
+              <Box flexGrow={1}>
+                <Typography variant="h2">{user.name}</Typography>
+              </Box>
+              <Box display="flex" gap={2}>
                 <Typography variant="body2">Rating: {seller.rating}</Typography>
-              </Grid>
-              <Grid item sm={1}>
                 <Typography variant="body2">Sales: {seller.numberOfSales}</Typography>
-              </Grid>
-            </Grid>
+              </Box>
+            </Box>
           </Box>
         </Box>
         <Box sx={{ width: "100%", bgcolor: "background.paper" }}>
@@ -115,7 +96,6 @@ export default function Seller({ params }) {
             <Tab label="Reviews" value="reviews" />
           </Tabs>
         </Box>
-        {/* <Box p={5}> */}
         {activeTab === "cards" && (
           <Grid container spacing={2}>
             <Grid item xs={2}></Grid>
@@ -131,11 +111,35 @@ export default function Seller({ params }) {
           </Grid>
         )}
         {activeTab === "about" && (
-          <Paper elevation={3} sx={{ p: 2 }}>
-            <Typography variant="body1">{user.address}</Typography>
-            <Typography variant="body1">{user.email}</Typography>
-          </Paper>
+          <Grid container spacing={2}>
+            <Grid item xs></Grid>
+            <Grid item xs={8}>
+              <Paper elevation={3} sx={{ px: 2, py: 5 }}>
+                <Box display="flex" gap={1} my={2}>
+                  <Typography variant="h4">Location:</Typography>
+                  <Typography variant="body1">here will be information about seller location</Typography>
+                </Box>
+                <Box display="flex" gap={1} my={2}>
+                  <Typography variant="h4">Seller since:</Typography>
+                  <Typography variant="body1">
+                    here will be information since than the seller is seeling cards
+                  </Typography>
+                </Box>
+                <Box display="flex" gap={1} my={2}>
+                  <Typography variant="h4">Other:</Typography>
+                  <Typography variant="body1">here will be other information about the seller</Typography>
+                </Box>
+              </Paper>
+            </Grid>
+            <Grid item xs></Grid>
+          </Grid>
         )}
+        {activeTab === "reviews" &&
+          seller.feedback.map((feedback, index) => (
+            <Paper key={index} sx={{ p: 2, my: 1 }}>
+              <Typography variant="body1">{feedback}</Typography>
+            </Paper>
+          ))}
       </Box>
       <Snackbar
         open={openError}
@@ -146,7 +150,6 @@ export default function Seller({ params }) {
           {errorMessage}
         </Alert>
       </Snackbar>
-      {/* </Box> */}
     </Container>
   );
 }
