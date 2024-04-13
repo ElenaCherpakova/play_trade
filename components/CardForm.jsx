abab8624b1f43725c575f76bd7aa275922ae34a1
Anna Pestova <106414157+AnnaPestova1@users.noreply.github.com>
2024-04-12 23:19:10 -0400

    remove negative numbers from cards quantity

--- components/CardForm.jsx
+++ components/CardForm.jsx
@@ -67,13 +67,13 @@ export default function CardForm({ cardValue, onSubmitForm }) {
     if (file) {
       setSelectedFile(file);
     }
-  }
+  };
 
   const handlePaperClick = () => {
     fileInputRef.current.click();
   };
 
-  const handleSubmit = async (e) => {
+  const handleSubmit = async e => {
     e.preventDefault();
 
     const { cardName, set, price, currency, shippingCost, description, conditions, quantity, available } =
@@ -293,6 +293,9 @@ export default function CardForm({ cardValue, onSubmitForm }) {
                   name="quantity"
                   type="number"
                   label="quantity"
+                  inputProps={{
+                    min: "0"
+                  }}
                   defaultValue={cardValue?.quantity}
                 />
                 <FormControl required fullWidth size="small">
