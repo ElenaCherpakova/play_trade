69cf7ed574e7d6f91c41ef15e9b787a96c6fcae9
Anna Pestova <106414157+AnnaPestova1@users.noreply.github.com>
2024-04-14 13:17:07 -0400

    fix error catching

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -79,7 +79,7 @@ export default function Profile() {
         setErrorMessage(data.message || "Failed to become a seller");
       }
     } catch (error) {
-      setErrorMessage(error.message || "An error occurred while updating");
+      setErrorMessage(error.toString() || "An error occurred while updating");
     }
     setLoading(false);
   };
