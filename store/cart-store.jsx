03cd107d08182c8fe063a81736c2cecd5279cc87
ElenaCherpakova <elena.cherpakova@gmail.com>
2024-04-14 16:50:50 -0400

    display count in the cart

--- store/cart-store.jsx
+++ store/cart-store.jsx
@@ -5,7 +5,7 @@ export const useCartStore = create(set => ({
   addToCart: product =>
     set(state => {
       if (product.available !== "available") {
-        return { cartItems: state.cartItems };
+        return { ...state };
       }
       const existingProductIndex = state.cartItems.findIndex(item => item._id === product._id);
       if (existingProductIndex !== -1) {
@@ -14,26 +14,30 @@ export const useCartStore = create(set => ({
         if (existingProduct.quantity < product.quantity) {
           existingProduct.quantity += 1;
         }
-        return { cartItems: newCartItems };
+        return { cartItems: newCartItems, count: newCartItems.reduce((total, item) => total + item.quantity, 0) };
       } else {
-        return { cartItems: [...state.cartItems, { ...product, quantity: 1, checked: true }] };
+        const newCartItems = [...state.cartItems, { ...product, quantity: 1, checked: true }];
+        return { cartItems: newCartItems, count: newCartItems.reduce((total, item) => total + item.quantity, 0) };
       }
     }),
   removeItemFromCart: productId =>
-    set(state => ({ cartItems: state.cartItems.filter(product => product._id !== productId) })),
-  clearCart: () => set({ cartItems: [] }),
+    set(state => {
+      const newCartItems = state.cartItems.filter(product => product._id !== productId);
+      return { cartItems: newCartItems, count: newCartItems.reduce((total, item) => total + item.quantity, 0) };
+    }),
+  clearCart: () => set({ cartItems: [], count: 0 }),
   handleCheck: index =>
     set(state => {
       const newCartItems = [...state.cartItems];
       newCartItems[index].checked = !newCartItems[index].checked;
-      return { cartItems: newCartItems };
+      return { cartItems: newCartItems, count: newCartItems.reduce((total, item) => total + item.quantity, 0) };
     }),
   handleQuantityChange: (index, newQuantity) =>
     set(state => {
       const newCartItems = [...state.cartItems];
       const targetItem = newCartItems[index];
       const newQty = Number(newQuantity);
-      console.log("newQty", newQty)
+      console.log("newQty", newQty);
       if (newQty > 0 && newQty <= targetItem.quantity) {
         targetItem.quantity = newQty;
       } else if (newQty > targetItem.quantity) {
@@ -41,6 +45,6 @@ export const useCartStore = create(set => ({
       } else if (newQty <= 0) {
         newCartItems.splice(index, 1);
       }
-      return { cartItems: newCartItems };
+      return { cartItems: newCartItems, count: newCartItems.reduce((total, item) => total + item.quantity, 0) };
     })
 }));
