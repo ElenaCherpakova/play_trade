2e125e7eabc48de6479d431f69059df95b33bcc2
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2024-04-17 12:26:28 -0700

    add loading useState boolean, add useEffect

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,28 @@
 "use state";
+"use client";
+import React, { useState, useEffect } from "react";
 import Home from "../components/Home"; // import Home component
+import Loader from "@/components/loader/Loader";
 
 export default function Page() {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 8000);
+  }, []);
+
   return (
     <>
-    <Home /> 
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          <Home />
+        </>
+      )}
     </>
   );
 }
