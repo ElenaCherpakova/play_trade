9b174c737dd5e64552243c610378681a21ed796b
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2024-04-17 13:00:59 -0700

    add box styling to center loader

--- components/loader/Loader.jsx
+++ components/loader/Loader.jsx
@@ -1,11 +1,20 @@
 import React from "react";
+import { Box } from "@mui/material";
 
 function Loader() {
   return (
-    <div>
+    <Box
+      width="10%"
+      height="10%"
+      sx={{
+        position: "absolute",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)"
+      }}>
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200">
-        <rect fill="#FFFFFF" width="20%" height="20%" />
-        <circle fill="#1462D5" stroke="#1462D5" stroke-width="13" r="15" cx="40" cy="65">
+        <rect fill="#FFFFFF" width="100%" height="100%" />
+        <circle fill="#1462D5" stroke="#1462D5" stroke-width="2" r="15" cx="40" cy="65">
           <animate
             attributeName="cy"
             calcMode="spline"
@@ -15,7 +24,7 @@ function Loader() {
             repeatCount="indefinite"
             begin="-.4"></animate>
         </circle>
-        <circle fill="#1462D5" stroke="#1462D5" stroke-width="13" r="15" cx="100" cy="65">
+        <circle fill="#1462D5" stroke="#1462D5" stroke-width="2" r="15" cx="100" cy="65">
           <animate
             attributeName="cy"
             calcMode="spline"
@@ -25,7 +34,7 @@ function Loader() {
             repeatCount="indefinite"
             begin="-.2"></animate>
         </circle>
-        <circle fill="#1462D5" stroke="#1462D5" stroke-width="13" r="15" cx="160" cy="65">
+        <circle fill="#1462D5" stroke="#1462D5" stroke-width="2" r="15" cx="160" cy="65">
           <animate
             attributeName="cy"
             calcMode="spline"
@@ -36,7 +45,7 @@ function Loader() {
             begin="0"></animate>
         </circle>
       </svg>
-    </div>
+    </Box>
   );
 }
 
