7c3464f588595536b3f39bca6c9a0647cb809024
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2024-04-21 14:05:22 -0700

    remove unused Loader logic.

--- app/page.jsx
+++ app/page.jsx
@@ -1,28 +1,10 @@
 "use state";
-// "use client";
-// import React, { useState, useEffect } from "react";
 import Home from "../components/Home"; // import Home component
-// import Loader from "@/components/loader/Loader";
 
 export default function Page() {
-  // const [loading, setLoading] = useState(false);
-
-  // useEffect(() => {
-  //   setLoading(true);
-  //   setTimeout(() => {
-  //     setLoading(false);
-  //   }, 3000);
-  // }, []);
-
   return (
     <>
-      {/* {loading ? (
-        <Loader />
-      ) : (
-        <> */}
       <Home />
-      {/* </>
-      )} */}
     </>
   );
 }
