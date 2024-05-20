21dda501a6c0b14ccb90934ae11634186e4c010f
ElenaCherpakova <elena.cherpakova@gmail.com>
2024-05-20 17:30:56 -0400

    connect google analytics and setup cookie consent

--- app/layout.jsx
+++ app/layout.jsx
@@ -9,7 +9,9 @@ import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
 
 import Navbar from "../components/Navbar";
 
+import CookieBanner from "@/components/CookieBanner";
 import Footer from "@/components/Footer";
+import GoogleAnalytics from "@/components/GoogleAnalytics";
 import { theme } from "@/styles/theme";
 import SessionProvider from "@/utils/SessionProvider";
 
@@ -25,6 +27,7 @@ export default async function RootLayout({ children }) {
 
   return (
     <html lang="en">
+      <GoogleAnalytics GA_MEASUREMENT_ID={process.env.GOOGLE_GA_MEASUREMENT_ID} />
       <body className={inter.className}>
         <SessionProvider session={session}>
           <AppRouterCacheProvider>
@@ -42,6 +45,7 @@ export default async function RootLayout({ children }) {
                     flexGrow: "1"
                   }}>
                   {children}
+                  <CookieBanner />
                 </Box>
               </main>
               <Footer />
